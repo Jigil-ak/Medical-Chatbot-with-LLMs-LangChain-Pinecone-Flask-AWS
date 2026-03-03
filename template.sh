64bcaf60b26acb1749f9d9c137fcc1387ce88e8a
Jigil AK <jigil.ak2003@gmail.com>
2026-03-03 12:24:50 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directoty
+mkdir -p src
+mkdir -p reasearch
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reasearch/trials.ipynb
+touch requirements.txt
+
+echo "dictionary file created successfully"
